03d6c6418875ece539c8f48aacf8f73a72403f29
Filipe <123255512+F-i-l-i-p-e@users.noreply.github.com>
2023-06-25 21:53:23 -0400

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,78 @@
+﻿// BloodPressure Enum
+public enum BloodPressureCategory
+{
+    Normal,
+    Elevated,
+    HypertensionStage1,
+    HypertensionStage2,
+    HypertensiveCrisis,
+    Unknown
+}
+
+// BMI Enum
+public enum BMICategory
+{
+    Underweight,
+    Normal,
+    Overweight,
+    Unknown
+}
+
+// Patient class
+public class Patient
+{
+    private string FirstName { get; set; } // First name
+    private string LastName { get; set; }  // Last name
+    private int Age { get; set; }  // Age
+    private double Weight { get; set; }  // Weight
+    private double Height { get; set; }  // Height
+
+    // Constructor
+    public Patient(string firstName, string lastName, int age, double weight, double height)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Weight = weight;
+        Height = height;
+    }
+
+    // BP calculation
+    public BloodPressureCategory CalculateBloodPressure(int systolic, int diastolic)
+    {
+        if (systolic < 120 && diastolic < 80) return BloodPressureCategory.Normal;
+        if (systolic >= 120 && systolic < 130 && diastolic < 80) return BloodPressureCategory.Elevated;
+        if ((systolic >= 130 && systolic < 140) || (diastolic >= 80 && diastolic < 90)) return BloodPressureCategory.HypertensionStage1;
+        if (systolic >= 140 || diastolic >= 90) return BloodPressureCategory.HypertensionStage2;
+        if (systolic > 180 || diastolic > 120) return BloodPressureCategory.HypertensiveCrisis;
+        return BloodPressureCategory.Unknown;
+    }
+
+    // BMI calculation
+    public double CalculateBMI()
+    {
+        double heightInMeter = Height / 100;
+        return Weight / (heightInMeter * heightInMeter);
+    }
+
+    // BMI status
+    public BMICategory GetBMIStatus(double bmi)
+    {
+        if (bmi < 18.5) return BMICategory.Underweight;
+        if (bmi >= 18.5 && bmi < 24.9) return BMICategory.Normal;
+        return BMICategory.Overweight;
+    }
+
+    // Info printing
+    public void PrintPatientInfo(int systolic, int diastolic)
+    {
+        Console.WriteLine($"Full Name: {FirstName} {LastName}");
+        Console.WriteLine($"Age: {Age} years");
+        Console.WriteLine($"Weight: {Weight} KG");
+        Console.WriteLine($"Height: {Height} CM");
+        Console.WriteLine($"Blood Pressure: {CalculateBloodPressure(systolic, diastolic)}");
+        double bmi = CalculateBMI();
+        Console.WriteLine($"BMI: {bmi}");
+        Console.WriteLine($"BMI Status: {GetBMIStatus(bmi)}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// Assignment 1 - ASP206-Group 8
+// Main Program File
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter patient's age: ");
+        int age = GetValidIntInput();  // Age input
+
+        Console.Write("Enter patient's first name: ");
+        string firstName = Console.ReadLine();  // Firstname input
+
+        Console.Write("Enter patient's last name: ");
+        string lastName = Console.ReadLine();  // Lastname input
+
+        Console.Write("Enter patient's weight (in KG): ");
+        double weight = GetValidDoubleInput();  // Weight input
+
+        Console.Write("Enter patient's height (in CM): ");
+        double height = GetValidDoubleInput();  // Height input
+
+        Console.Write("Enter patient's Systolic Blood Pressure: ");
+        int systolic = GetValidIntInput();  // Systolic input
+
+        Console.Write("Enter patient's Diastolic Blood Pressure: ");
+        int diastolic = GetValidIntInput();  // Diastolic input
+
+        Patient patient = new Patient(firstName, lastName, age, weight, height);  // Patient creation
+        patient.PrintPatientInfo(systolic, diastolic);  // Print information
+    }
+
+    static int GetValidIntInput()
+    {
+        int output;
+        while (!int.TryParse(Console.ReadLine(), out output) || output < 0)
+        {
+            Console.WriteLine("Invalid input. Enter a positive number.");
+        }
+        return output;  // Return integer
+    }
+
+    static double GetValidDoubleInput()
+    {
+        double output;
+        while (!double.TryParse(Console.ReadLine(), out output) || output < 0.0)
+        {
+            Console.WriteLine("Invalid input. Enter a positive number.");
+        }
+        return output;  // Return double
+    }
+}
